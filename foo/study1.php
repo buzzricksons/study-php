1770303b7143716a7052df7006c8979e6129b8e2
Hyungcheol Kim <hcgoon@gmail.com>
2018-04-15 17:49:38 +0900

    edit study.php

--- foo/study1.php
+++ foo/study1.php
@@ -5,8 +5,16 @@
  * Date: 2018/04/06
  * Time: 18:53
  */
-$zipcode = trim($_POST['zipcode']);
+$zipcode = '114-0033';
+//$zipcode = trim($_POST['zipcode']);
 $zip_length = strlen($zipcode);
 if ($zip_length != 5) {
     print "5555";
 }
+
+nl2br("\n");
+
+$preapration = 'hot';
+print "meat with {$preapration} !!!";
+
+

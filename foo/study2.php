848570abdd5cf13ca1e136dc25f6162fd7a227d7
Hyungcheol Kim <hcgoon@gmail.com>
2018-05-04 16:44:01 +0900

    add php source in study2.php

--- foo/study2.php
+++ foo/study2.php
@@ -1,7 +1,9 @@
 <?php
 $vegetables1 = array('corn' => 'yellow','beet' => 'red', 'carrot' => 'orange');
 $vegetables2 = ['corn' => 'yellow','beet' => 'red', 'carrot' => 'orange'];
-print $vegetables;
+print $vegetables1['corn'].PHP_EOL;
+print $vegetables2['corn'].PHP_EOL;
+print count($vegetables2);
 
 
 

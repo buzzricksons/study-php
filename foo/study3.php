54aecd3e3ecada2c0968ed2663788db250472ffe
Hyungcheol Kim <hcgoon@gmail.com>
2018-05-04 19:01:46 +0900

    add example source in study3.php

--- foo/study3.php
+++ foo/study3.php
@@ -13,11 +13,33 @@ p_header();
 print "execute\n";
 p_footer();
 p_header2('red');
+p_header2();
 
 function p_footer() {
     print "foot!\n";
 }
 
-function p_header2($color) {
+function p_header2($color = 'yellow') {
     print "color is $color\n";
 }
+
+$dinner = 'hambuger';
+function a_dinner() {
+    $dinner = 'bugerking';
+    print $dinner."\n";
+    print $GLOBALS['dinner']."\n";
+    $GLOBALS['dinner'] = 'rice';
+    print $GLOBALS['dinner']."\n";
+    $GLOBALS['dinner'].= 'kimchi';
+    print $GLOBALS['dinner']."\n";
+}
+a_dinner();
+
+$dinner2 = 'cola';
+function b_dinner() {
+    global $dinner2;
+    print "before:$dinner2\n";
+    $dinner2 = 'water';
+    print "after:$dinner2\n";
+}
+b_dinner();
